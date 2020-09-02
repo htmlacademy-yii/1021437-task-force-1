5b61f207c2565fa4b27610f67b903be643dddb79
Bogdan <moiseenkob@gmail.com>
2020-09-02 20:25:32 +0300

    Изменил имя для директории

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 
-use Task\handler\Task;
+use Task\classes\Task;
 
 require_once 'vendor/autoload.php';
 
--- src/classes/Task.php
+++ src/classes/Task.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Task\handler;
+namespace Task\classes;
 
 class Task
 {
